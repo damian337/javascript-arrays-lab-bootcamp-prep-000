807fef737fc405d9a7425f78c6c8fbe105da0492
Damian Guillory <guillorywdamian@gmail.com>
2017-10-05 00:25:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-    var aKitten = [`${name}`, ...kittens];
+    var aKitten = [...kittens], `${name}`;
     return aKitten;
 }
